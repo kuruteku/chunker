67100f0e2dce56bedf7e1a419f1dd383d9e79c12
Jan Svec <lphaozn@gmail.com>
2014-04-09 20:48:47 +0200

    1.3.22 [BUGFIX] Disabled the accordance of subject and ending

--- chunker.rb
+++ chunker.rb
@@ -7,8 +7,13 @@ end
 class Chunker
   def initialize
     @subject = ["Dan","Píro","Canale","Rak","Zymonová","Ender Dragon","Creeper","Jolanda","Kuruteku_09","Keralis","Miley Cyrus","Mumbo","Mike Shinoda","Lindsey Stirling","Superpennys139","Herout","Nemčoková","Sakalová","Štěpánek","Gynekolog","Prezident Semen","Andršová","Exner","Niňuňu","Justin Bieber","Roko","Metrix","SethBling","Dragnoz"]
-    @verb = ["se dělá","si mastí","onanuje","znásilňuje Mařku","znásilňuje Ivonu","sleduje porno","jí majonézu","stříká do sklenice od okurkového salátu","honí buřtíka","vraždí učitele","twerkuje","líže kladivo","mlátí do klávesnice a druhou rukou s hladí pubické ochlupení","olizuje plakát s fotkou Dana","trhá učebnici němčiny","poslouchá Pe Cimpoi"]
-    @ending = ["v lednici","na záchodě","ve sborovně","v kabinetě fyziky","na fairwayi","u Dana doma","v Supermarketu","ve stánku s kuřaty","na Fortune Islandu","v Polsku","na koncertě Lindsey Stirling","za tvými zády","na střeše Danova domu","v bordelu","v Drážďanech","ve vlaku","v Biografu Modřany","v posteli","na balkóně","v taxíku","na pláži","v učebně IKT"]
+    @verb = ["se dělá","si mastí","onanuje","znásilňuje Mařku","znásilňuje Ivonu","sleduje porno","jí majonézu","stříká do sklenice od okurkového salátu","honí buřtíka","vraždí učitele","twerkuje","líže kladivo","mlátí do klávesnice a druhou rukou s hladí pubické ochlupení","olizuje plakát s fotkou Dana","trhá učebnici němčiny","poslouchá Pe Cimpoi","se nechává mlátit Vlasákem","ničí Vlasákovo auto","hází balalajkou"]
+    @ending = ["v lednici","na záchodě","ve sborovně","v kabinetě fyziky","na fairwayi","u Dana doma","v Supermarketu","ve stánku s kuřaty","na Fortune Islandu","v Polsku","na koncertě Lindsey Stirling","za tvými zády","na střeše Danova domu","v bordelu","v Drážďanech","ve vlaku","v Biografu Modřany","v posteli","na balkóně","v taxíku","na pláži","v učebně IKT","ve Vlasákově autě","na greenu"]
+  end
+
+  def sub_end
+    @subject_sam = @subject.sample
+    @ending_sam = @ending.sample
   end
 
   def roll
@@ -30,8 +35,15 @@ class Chunker
     end
 
     split_i = split[1].to_i
+    sub_end
     split_i.times do
-      puts "#{@subject.sample} #{@verb.sample} #{@ending.sample}."
+      sub_end
+      if !@ending_sam.include? @subject_sam
+        puts "#{@subject_sam} #{@verb.sample} #{@ending_sam}."
+      else
+        sub_end
+        puts "#{@subject_sam} #{@verb.sample} #{@ending_sam}."
+      end
 
     end
 
@@ -52,6 +64,7 @@ class Chunker
       end
     end
   end
+
 end
 
 chunker = Chunker.new
