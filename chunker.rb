e5cffb59f226ca42df829d9b7629377127c5dbf5
Jan Svec <lphaozn@gmail.com>
2014-04-10 20:33:45 +0200

    [DATA]

--- chunker.rb
+++ chunker.rb
@@ -6,9 +6,9 @@ end
 
 class Chunker
   def initialize
-    @subjects = ["Dan","Píro","Canale","Rak","Zymonová","Ender Dragon","Creeper","Jolanda","Kuruteku_09","Keralis","Miley Cyrus","Mumbo","Mike Shinoda","Lindsey Stirling","Superpennys139","Herout","Nemčoková","Sakalová","Štěpánek","Gynekolog","Prezident Semen","Andršová","Exner","Niňuňu","Justin Bieber","Roko","Metrix","SethBling","Dragnoz"]
-    @verbs = ["se dělá","si mastí","onanuje","znásilňuje Mařku","znásilňuje Ivonu","sleduje porno","jí majonézu","stříká do sklenice od okurkového salátu","honí buřtíka","vraždí učitele","twerkuje","líže kladivo","mlátí do klávesnice a druhou rukou s hladí pubické ochlupení","olizuje plakát s fotkou Dana","trhá učebnici němčiny","poslouchá Pe Cimpoi","se nechává mlátit Vlasákem","ničí Vlasákovo auto","hází balalajkou"]
-    @endings = ["v lednici","na záchodě","ve sborovně","v kabinetě fyziky","na fairwayi","u Dana doma","v Supermarketu","ve stánku s kuřaty","na Fortune Islandu","v Polsku","na koncertě Lindsey Stirling","za tvými zády","na střeše Danova domu","v bordelu","v Drážďanech","ve vlaku","v Biografu Modřany","v posteli","na balkóně","v taxíku","na pláži","v učebně IKT","ve Vlasákově autě","na greenu"]
+    @subjects = ["Dan","Píro","Canale","Rak","Zymonová","Ender Dragon","Creeper","Jolanda","Kuruteku_09","Keralis","Miley Cyrus","Mumbo","Mike Shinoda","Lindsey Stirling","Superpennys139","Herout","Nemčoková","Sakalová","Štěpánek","Anonymní gynekolog","Prezident Semen","Andršová","Exner","Nina Nováková","Justin Bieber","Roko","Metrix","SethBling","Dragnoz","Kuchařka od okýnka číslo dvě","Hospodářka","Svatá trojce","Adam Urbánek",""]
+    @verbs = ["se dělá","si mastí","onanuje","znásilňuje Mařku","znásilňuje Ivonu","sleduje porno","jí majonézu","stříká do sklenice od okurkového salátu","honí buřtíka","vraždí učitele","twerkuje","líže kladivo","mlátí do klávesnice a druhou rukou s hladí pubické ochlupení","olizuje plakát s fotkou Dana","trhá učebnici němčiny","poslouchá Pe Cimpoi","se nechává mlátit Vlasákem","ničí Vlasákovo auto","hází balalajkou","pláče u indické telenovely o krásné Penisnazádě, jíž osud připravil strastiplnou cestu z vrcholu až na dno, natáčené převážně"]
+    @endings = ["v lednici","na záchodě","ve sborovně","v kabinetě fyziky","na fairwayi","u Dana doma","v Supermarketu","ve stánku s kuřaty","na Fortune Islandu","v Polsku","na koncertě Lindsey Stirling","za tvými zády","na střeše Danova domu","v bordelu","v Drážďanech","ve vlaku","v Biografu Modřany","v posteli","na balkóně","v taxíku","na pláži","v učebně IKT","ve Vlasákově autě","na greenu","ve spawnchuncích","v Serekundě","na podvozku auta paní profesorky Zymonové","v Netheru","v hotelu Jednota","v Ivonově","v jaderném reaktoru","v SSSR"]
   end
   
  
@@ -58,7 +58,7 @@ class Chunker
        
 
     while true
-      puts "Type 'roll number' to roll the dice and get your sentences! Or you can type exit to exit..."
+      puts "Type 'roll number' to roll the dice and get your sentences!\nType exit to quit."
       command = gets.chomp
       case command
       when "exit"
